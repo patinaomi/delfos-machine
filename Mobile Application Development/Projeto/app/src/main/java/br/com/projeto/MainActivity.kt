eaf0d978fad81891ce708bc8fb54e4cc736a3d27
Patricia Naomi <patricianaomiyama@gmail.com>
2024-10-07 02:24:28 -0300

    FEAT: Validação dos dados

--- Mobile Application Development/Projeto/app/src/main/java/br/com/projeto/MainActivity.kt
+++ Mobile Application Development/Projeto/app/src/main/java/br/com/projeto/MainActivity.kt
@@ -2,16 +2,24 @@ package br.com.projeto
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // Declaração de variáveis
     private lateinit var voltarImage: ImageView
     private lateinit var esqueciSenhaTextView: TextView
+    private lateinit var acessarButton : Button
+    private lateinit var loginEditText : EditText
+    private lateinit var senhaEditText : EditText
+    private lateinit var erroLoginTextView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +32,54 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // Inicialização de variáveis
         voltarImage = findViewById(R.id.img_back)
+        esqueciSenhaTextView = findViewById(R.id.tv_esqueci_senha)
+        loginEditText = findViewById(R.id.et_login)
+        senhaEditText = findViewById(R.id.et_senha)
+        acessarButton = findViewById(R.id.bt_acessar)
+        erroLoginTextView = findViewById(R.id.tv_erro_login)
+
         voltarImage.setOnClickListener {
             val intent = Intent(this, InicioActivity::class.java)
             startActivity(intent)
         }
 
-        esqueciSenhaTextView = findViewById(R.id.tv_esqueci_senha)
         esqueciSenhaTextView.setOnClickListener {
             val intent = Intent(this, RecuperarSenhaActivity::class.java)
             startActivity(intent)
+        }
+
+        acessarButton.setOnClickListener {
+            validarLogin(loginEditText.text.toString(), senhaEditText.text.toString(), erroLoginTextView)
+        }
+    }
+
+    // Como não trabalhamos ainda com persistência no banco, simulamos um login com CPF e senha
+    private fun validarLogin(cpf: String, senha: String, erroLoginTextView: TextView) {
+        if(cpf.isBlank() || senha.isBlank()) {
+            // Caso 1 - CPF ou senha não preenchidos
+            erroLoginTextView.text = "CPF e senha são obrigatórios"
+            erroLoginTextView.visibility = TextView.VISIBLE
+
+        } else if (cpf == "123456789" && senha == "123456") {
+            // Caso 2 - Login efetuado com sucesso
+            AlertDialog.Builder(this)
+                .setTitle("Login Efetuado")
+                .setMessage("Login efetuado com sucesso!")
+                .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                .show()
+
+            erroLoginTextView.visibility = TextView.GONE
+        } else if (cpf == "123456789" && senha != "123456") {
+            // Caso 3 - CPF correto, mas senha incorreta
+            erroLoginTextView.text = "Senha inválida"
+            erroLoginTextView.visibility = TextView.VISIBLE
+
+        } else {
+            // Caso 4 - CPF incorreto
+            erroLoginTextView.text = "CPF ou Carteirinha não está cadastrado no banco"
+            erroLoginTextView.visibility = TextView.VISIBLE
         }
     }
 }

7e713c1e618ecb99d39203630f617198cba52180
Patricia Naomi <patricianaomiyama@gmail.com>
2024-10-07 01:49:46 -0300

    FEAT: Activity Esqueci minha senha

--- Mobile Application Development/Projeto/app/src/main/java/br/com/projeto/MainActivity.kt
+++ Mobile Application Development/Projeto/app/src/main/java/br/com/projeto/MainActivity.kt
@@ -3,28 +3,36 @@ package br.com.projeto
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var voltarImage: ImageView
+    private lateinit var esqueciSenhaTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Ajustar as margens para insets do sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // Configurar clique na imagem para voltar à página principal
-        val myImage: ImageView = findViewById(R.id.img_back)
-        myImage.setOnClickListener {
+        voltarImage = findViewById(R.id.img_back)
+        voltarImage.setOnClickListener {
             val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
+        }
+
+        esqueciSenhaTextView = findViewById(R.id.tv_esqueci_senha)
+        esqueciSenhaTextView.setOnClickListener {
+            val intent = Intent(this, RecuperarSenhaActivity::class.java)
             startActivity(intent)
         }
     }
